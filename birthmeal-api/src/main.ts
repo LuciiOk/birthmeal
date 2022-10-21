5ebbe242a3683bd0c9e0f2b926b5a958c670ab0b
Luciano Portales <lportales655@gmail.com>
2022-10-20 21:15:06 -0300

    added procfile

--- birthmeal-api/src/main.ts
+++ birthmeal-api/src/main.ts
@@ -14,6 +14,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
