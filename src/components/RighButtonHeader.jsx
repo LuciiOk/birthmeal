ddcc84b14418123450b60212db73e08c0efb7331
Luciano Portales <lportales655@gmail.com>
2022-11-10 18:07:47 -0300

    removed border width

--- src/components/RighButtonHeader.jsx
+++ src/components/RighButtonHeader.jsx
@@ -130,8 +130,6 @@ const styles = StyleSheet.create({
     width: "100%",
     justifyContent: "center",
     alignItems: "center",
-    borderBottomWidth: 1,
-    borderBottomColor: COLORS.light,
   },
   text: {
     fontSize: 20,
