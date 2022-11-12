b33eecc339ef0c793c85a2aba6ff813bd23e434e
Luciano Portales <lportales655@gmail.com>
2022-11-11 22:09:22 -0300

    map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,140 +2,30 @@ import React from "react";
 import { StyleSheet } from "react-native";
 import MapView from "react-native-maps";
 import PropTypes from "prop-types";
+import { COLORS } from "../constants/colorSchema";
 
 const Map = ({ latitude, longitude, ...props }) => {
   const mapStyle = [
     {
       featureType: "all",
-      elementType: "geometry",
+      elementType: "all",
       stylers: [
         {
+          saturation: "32",
+        },
+        {
+          lightness: "-3",
+        },
+        {
           visibility: "on",
+        },
+        {
+          weight: "1.18",
         },
       ],
     },
     {
       featureType: "administrative",
-      elementType: "all",
-      stylers: [
-        {
-          visibility: "on",
-        },
-      ],
-    },
-    {
-      featureType: "administrative.country",
-      elementType: "geometry.stroke",
-      stylers: [
-        {
-          color: "#afafaf",
-        },
-      ],
-    },
-    {
-      featureType: "administrative.province",
-      elementType: "all",
-      stylers: [
-        {
-          visibility: "off",
-        },
-      ],
-    },
-    {
-      featureType: "administrative.land_parcel",
-      elementType: "geometry",
-      stylers: [
-        {
-          visibility: "on",
-        },
-      ],
-    },
-    {
-      featureType: "landscape",
-      elementType: "all",
-      stylers: [
-        {
-          visibility: "on",
-        },
-      ],
-    },
-    {
-      featureType: "landscape.man_made",
-      elementType: "geometry",
-      stylers: [
-        {
-          saturation: "25",
-        },
-        {
-          lightness: "46",
-        },
-        {
-          gamma: "1.26",
-        },
-      ],
-    },
-    {
-      featureType: "landscape.man_made",
-      elementType: "geometry.fill",
-      stylers: [
-        {
-          visibility: "on",
-        },
-        {
-          hue: "#ff0000",
-        },
-      ],
-    },
-    {
-      featureType: "landscape.man_made",
-      elementType: "geometry.stroke",
-      stylers: [
-        {
-          hue: "#ff0000",
-        },
-      ],
-    },
-    {
-      featureType: "landscape.natural",
-      elementType: "geometry",
-      stylers: [
-        {
-          visibility: "on",
-        },
-      ],
-    },
-    {
-      featureType: "poi",
-      elementType: "all",
-      stylers: [
-        {
-          visibility: "on",
-        },
-      ],
-    },
-    {
-      featureType: "poi",
-      elementType: "geometry",
-      stylers: [
-        {
-          visibility: "on",
-        },
-        {
-          color: "#b6edb6",
-        },
-        {
-          weight: "0.33",
-        },
-        {
-          lightness: "16",
-        },
-        {
-          saturation: "-17",
-        },
-      ],
-    },
-    {
-      featureType: "poi.attraction",
       elementType: "labels",
       stylers: [
         {
@@ -144,62 +34,8 @@ const Map = ({ latitude, longitude, ...props }) => {
       ],
     },
     {
-      featureType: "poi.business",
-      elementType: "geometry",
-      stylers: [
-        {
-          visibility: "on",
-        },
-        {
-          color: "#ede4e4",
-        },
-        {
-          saturation: "-63",
-        },
-        {
-          gamma: "0.38",
-        },
-        {
-          lightness: "61",
-        },
-      ],
-    },
-    {
-      featureType: "poi.medical",
-      elementType: "geometry.fill",
-      stylers: [
-        {
-          color: "#f4b4b4",
-        },
-        {
-          saturation: "-16",
-        },
-        {
-          lightness: "20",
-        },
-      ],
-    },
-    {
-      featureType: "poi.medical",
-      elementType: "labels.icon",
-      stylers: [
-        {
-          visibility: "on",
-        },
-      ],
-    },
-    {
-      featureType: "poi.park",
-      elementType: "geometry",
-      stylers: [
-        {
-          visibility: "on",
-        },
-      ],
-    },
-    {
-      featureType: "poi.park",
-      elementType: "labels.text",
+      featureType: "landscape",
+      elementType: "labels",
       stylers: [
         {
           visibility: "off",
@@ -207,146 +43,32 @@ const Map = ({ latitude, longitude, ...props }) => {
       ],
     },
     {
-      featureType: "poi.school",
-      elementType: "geometry",
-      stylers: [
-        {
-          visibility: "on",
-        },
-        {
-          color: "#fbc99b",
-        },
-        {
-          lightness: "12",
-        },
-        {
-          gamma: "2.39",
-        },
-      ],
-    },
-    {
-      featureType: "road.highway",
+      featureType: "landscape.man_made",
       elementType: "all",
       stylers: [
         {
-          color: "#fdcb9b",
+          saturation: "-70",
+        },
+        {
+          lightness: "14",
         },
       ],
     },
     {
-      featureType: "road.highway",
-      elementType: "geometry",
-      stylers: [
-        {
-          visibility: "on",
-        },
-      ],
-    },
-    {
-      featureType: "road.highway",
-      elementType: "geometry.fill",
-      stylers: [
-        {
-          visibility: "on",
-        },
-        {
-          color: "#f2af67",
-        },
-        {
-          lightness: "47",
-        },
-        {
-          saturation: "64",
-        },
-        {
-          gamma: "0.95",
-        },
-      ],
-    },
-    {
-      featureType: "road.highway",
-      elementType: "geometry.stroke",
-      stylers: [
-        {
-          visibility: "on",
-        },
-        {
-          saturation: "-18",
-        },
-        {
-          lightness: "61",
-        },
-        {
-          gamma: "7.93",
-        },
-        {
-          weight: "0.30",
-        },
-        {
-          color: "#db8828",
-        },
-      ],
-    },
-    {
-      featureType: "road.highway",
-      elementType: "labels.text",
-      stylers: [
-        {
-          weight: "0.79",
-        },
-        {
-          gamma: "3.19",
-        },
-        {
-          lightness: "-73",
-        },
-        {
-          saturation: "-76",
-        },
-        {
-          visibility: "on",
-        },
-      ],
-    },
-    {
-      featureType: "road.highway",
-      elementType: "labels.text.fill",
-      stylers: [
-        {
-          invert_lightness: true,
-        },
-        {
-          weight: "2.90",
-        },
-        {
-          color: "#786e6e",
-        },
-      ],
-    },
-    {
-      featureType: "road.highway",
-      elementType: "labels.text.stroke",
-      stylers: [
-        {
-          visibility: "off",
-        },
-      ],
-    },
-    {
-      featureType: "road.highway",
-      elementType: "labels.icon",
+      featureType: "poi",
+      elementType: "labels",
       stylers: [
         {
           visibility: "off",
         },
         {
-          color: "#5da554",
+          lightness: "5",
         },
       ],
     },
     {
-      featureType: "road.arterial",
-      elementType: "geometry",
+      featureType: "road",
+      elementType: "labels",
       stylers: [
         {
           visibility: "on",
@@ -354,8 +76,8 @@ const Map = ({ latitude, longitude, ...props }) => {
       ],
     },
     {
-      featureType: "road.local",
-      elementType: "geometry",
+      featureType: "transit",
+      elementType: "labels",
       stylers: [
         {
           visibility: "on",
@@ -364,77 +86,92 @@ const Map = ({ latitude, longitude, ...props }) => {
     },
     {
       featureType: "water",
-      elementType: "geometry.fill",
+      elementType: "all",
       stylers: [
         {
-          color: "#7fd4ef",
+          saturation: "100",
         },
         {
-          saturation: "13",
-        },
-        {
-          lightness: "-3",
-        },
-        {
-          gamma: "1.13",
-        },
-        {
-          weight: "0.30",
-        },
-      ],
-    },
-    // remove labels
-    {
-      featureType: "administrative",
-      elementType: "labels",
-      stylers: [
-        {
-          visibility: "off",
+          lightness: "-14",
         },
       ],
     },
     {
-      featureType: "administrative.country",
+      featureType: "water",
       elementType: "labels",
       stylers: [
         {
-          visibility: "off",
+          lightness: "12",
         },
       ],
     },
+    // change color of roads
     {
-      featureType: "administrative.province",
-      elementType: "labels",
+      featureType: "road",
+      elementType: "geometry",
       stylers: [
         {
-          visibility: "off",
+          color: `${COLORS.light}`,
         },
       ],
     },
+    // water color
     {
-      featureType: "administrative.locality",
-      elementType: "labels",
+      featureType: "water",
+      elementType: "geometry",
       stylers: [
         {
-          visibility: "off",
+          color: `${COLORS.frost2}`,
         },
       ],
     },
-    {
-      featureType: "administrative.neighborhood",
-      elementType: "labels",
-      stylers: [
-        {
-          visibility: "off",
-        },
-      ],
-    },
+    // change color of labels
     {
       featureType: "poi",
       elementType: "labels",
       stylers: [
         {
           visibility: "off",
+        },
+      ],
+    },
+    // change color of labels
+    {
+      featureType: "poi",
+      elementType: "labels.text.fill",
+      stylers: [
+        {
+          color: COLORS.dark,
+        },
+      ],
+    },
+    // change color of street labels
+    {
+      featureType: "road",
+      elementType: "labels.text.fill",
+      stylers: [
+        {
+          color: COLORS.dark,
+        },
+      ],
+    },
+    // change color of metro stations
+    {
+      featureType: "transit.station",
+      elementType: "labels.text.fill",
+      stylers: [
+        {
+          color: COLORS.dark,
+        },
+      ],
+    },
+    // change color of metro stations icons
+    {
+      featureType: "transit.station",
+      elementType: "geometry",
+      stylers: [
+        {
+          color: COLORS.dark,
         },
       ],
     },
