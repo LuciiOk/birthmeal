c837920bcd1e19fde1198540ef7da2dcccf6f1aa
Luciano Portales <lportales655@gmail.com>
2022-11-11 21:44:04 -0300

    stles filters

--- src/containers/FiltersContainer.jsx
+++ src/containers/FiltersContainer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, View, FlatList } from "react-native";
+import { View, FlatList } from "react-native";
 import PropTypes from "prop-types";
 
 import Text from "../components/Text";
@@ -7,7 +7,7 @@ import FiltersCards from "../components/FiltersCards";
 
 const FiltersContainer = ({ filters, setQuery, categoriesSelected }) => {
   return (
-    <View style={styles.header}>
+    <View>
       <Text
         styles={{
           width: "80%",
@@ -34,18 +34,12 @@ const FiltersContainer = ({ filters, setQuery, categoriesSelected }) => {
           />
         }
         horizontal
-        style={styles.list}
       />
       <Text text="Establecimientos" bold title />
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  header: {
-    marginHorizontal: 10,
-  },
-});
 
 FiltersContainer.propTypes = {
   filters: PropTypes.array,
