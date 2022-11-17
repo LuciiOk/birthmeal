c4f749e1d2d8daa43fe81d4b5c140cebe2851cb2
Luciano Portales <lportales655@gmail.com>
2022-11-16 22:16:18 -0300

    error image

--- src/Errors/ErrorBoundary.jsx
+++ src/Errors/ErrorBoundary.jsx
@@ -21,7 +21,7 @@ class ErrorBoundary extends React.Component {
       return (
         <View style={styles.container}>
           <Image
-            source={require("./../../assets/images/Burger-Sleeping.png")}
+            source={require("./../../assets/images/Error.png")}
             style={styles.image}
           />
           <Text
